66efa9482b07158b363f257657f8b96e46c03d00
changshuai <changs@bsoft.com.cn>
2024-05-09 20:41:05 +0800

    --

--- pc-app/src/App.vue
+++ pc-app/src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
+import Header from './view/components/Header'
+
+const fontSizeChange = () => {
+  document.body.style.fontSize = document.documentElement.clientWidth / 100 + 'px'
+}
+onMounted(() => {
+  window.addEventListener('resize', fontSizeChange)
+})
+onUnmounted(() => window.removeEventListener('resize', fontSizeChange))
+fontSizeChange()
 </script>
 
 <template>
+  <Header></Header>
   <router-view />
 </template>
 
--- pc-app/src/view/components/Header/index.vue
+++ pc-app/src/view/components/Header/index.vue
@@ -1,13 +1,67 @@
 <template>
-  <header>
-
+  <!-- Header -->
+  <header id="header" class="flex jc-sa">
+    <div class="logo">灵启云</div>
+    <ul class="nav flex jc-s">
+      <li v-for="item in navList" @click="changeNav(item)" :class="item.active && 'active'">{{ item.name }}</li>
+    </ul>
   </header>
 </template>
 
 <script setup>
+const router = useRouter();
 
+const navList = ref([{ name: '首页', active: true, path: 'index' }, { name: '关于我们', active: false, path: 'about' }])
+
+const changeNav = (item) => {
+  console.log(navList, item);
+
+  navList.value = navList.value.map((v) => {
+    return { ...v, active: item.name === v.name ? true : false }
+  }
+  )
+  router.push({ path: '/' + item.path })
+}
 </script>
 
 <style lang="scss" scoped>
-header {}
+#header {
+  font-size: 14px;
+  font-weight: bold;
+  height: 60px;
+  background: var(--theme-bule-dark);
+  color: var(--font-color01);
+
+  .logo {
+    width: 150px;
+    height: 50px;
+    margin: 0 15%;
+    line-height: 50px;
+    font-size: 20px;
+    background: url('../../../assets/images/logo.png') no-repeat left;
+    background-size: 55.2px 39.5px;
+    padding-left: 60px;
+  }
+
+  .nav {
+    flex: 1;
+
+    li {
+      margin: 0 10px;
+      cursor: pointer;
+      padding: 6px 8px;
+      border-bottom: 1px solid transparent;
+      transition: color, border-color .3s linear;
+
+      &.active {
+        color: var(--theme-orange);
+        border-bottom-color: var(--theme-orange);
+      }
+
+      &:hover {
+        color: var(--theme-orange);
+      }
+    }
+  }
+}
 </style>
